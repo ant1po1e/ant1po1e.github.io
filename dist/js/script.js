d488e1ab3c8dd0851887892952688b0aceb6e20e
Yudha Pratama <symamaxim@gmail.com>
2023-08-15 17:02:06 +0700

    fixed some stuff

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,25 +20,21 @@ hamburger.addEventListener('click', function() {
 
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbx0eCzzKaKtZ2gIwprnJ-5xtkpoM0kqcLv5uoVVUVZZMgIsJ6Fn6YHJ5OzUIOcpIcJV/exec";
-const form = document.forms["Ant1po1e-contact-form"];
-const btnSend = document.querySelector(".btn-send");
-const btnLoading = document.querySelector(".btn-loading");
-const myAlert = document.querySelector(".alert");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    btnLoading.classList.toggle("d-none");
-    btnSend.classList.toggle("d-none");
-        fetch(scriptURL, {
-            method: "POST",
-            body: new FormData(form),
-        })
-    .then((response) => {
-        btnLoading.classList.toggle("d-none");
-        btnSend.classList.toggle("d-none");
-        myAlert.classList.toggle("d-none");
-        form.reset();
-        console.log("Success!", response);
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
+        const form = document.forms["Ant1po1e-contact-form"];
+        const btnSend = document.querySelector(".btn-send");
+        const alert = document.querySelector(".alert")
+        
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            alert.classList.add('hidden')
+                fetch(scriptURL, {
+                    method: "POST",
+                    body: new FormData(form),
+                })
+            .then((response) => {
+                alert.classList.remove('hidden')
+                form.reset();
+                console.log("Success!", response);
+            })
+            .catch((error) => console.error("Error!", error.message));
+        });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/html/*.html'],
+  content: ['index.html'],
   theme: {
     container: {
       center: true,
