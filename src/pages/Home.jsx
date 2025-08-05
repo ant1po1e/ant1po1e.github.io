e4ca03a6e38df24b1517ea511c9496a384211c14
ant1po1e <symamaxim@gmail.com>
2025-08-05 13:40:06 +0700

    remove youtube feed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { Sidebar } from "../components/Sidebar";
 import { HeroSection } from "../components/HeroSection";
 import { Footer } from "../components/Footer";
-import { YouTubeFeed } from "../components/YouTubeFeed";
 import { useEffect } from "react";
 
 export const Home = () => {
@@ -12,7 +11,7 @@ export const Home = () => {
 	return (
 		<main className="relative z-10 max-w-[1200px] mx-auto px-6 pt-10 pb-6 min-h-[calc(100vh-72px)]">
 			<Sidebar />
-			<YouTubeFeed />
+			<HeroSection />
 			<Footer />
 		</main>
 	);
