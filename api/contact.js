c26514308ea2f12b7f2f90ee4049d69b0b70013c
ant1po1e <symamaxim@gmail.com>
2025-08-06 08:31:23 +0700

    fix contact

---
+++ api/contact.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+	if (req.method !== "POST") {
+		return res.status(405).json({ error: "Method not allowed" });
+	}
+
+	const scriptURL = process.env.CONTACT_FORM_SCRIPT;
+
+	try {
+		const response = await fetch(scriptURL, {
+			method: "POST",
+			body: req.body,
+			headers: {
+				"Content-Type": req.headers["content-type"],
+			},
+		});
+
+		const text = await response.text();
+
+		if (response.ok) {
+			return res.status(200).json({ message: "Success", text });
+		} else {
+			return res.status(500).json({ message: "Google Script Error", text });
+		}
+	} catch (err) {
+		console.error("Error:", err);
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
+}
