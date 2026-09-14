1c371c85d72ddb8c8a39f539a3a773f02bf88c3d
ant1po1e <symamaxim@gmail.com>
2026-09-14 17:50:50 +0700

    fix error

--- api/me.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { verifyAuth } from './_lib/auth';
-import { cookieReader } from './_lib/cookies';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const authenticated = verifyAuth(cookieReader(req));
-  return res.status(200).json({ authenticated });
-}
